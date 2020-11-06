6b04c2e76a6fcad51c7ebb09dab9cdfba4b1b148
Donut3339 <39208955+Donut3339@users.noreply.github.com>
2020-11-06 10:40:24 +0700

    Update WhitelistServer.js

--- WhitelistServer.js
+++ WhitelistServer.js
@@ -2,13 +2,34 @@
 const express = require("express")
 const app = express()
 
-// use the express-static middleware
-app.use(express.static("public"))
+const Keys = ["123", "simplewhitelist"]; // our key database of course you can replace it with a query prepared request to a mysql data base or mongodb
+const secretKey2 = "E);{Q6_<bkrEo;KEKWzlFoopANJai?}5vyus3l@>+?=>O}uL-(A}M/PJ`w";
+const Crypto = require("crypto");
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+function hmac(secret, data){
+    const hash = Crypto.createHash("sha512");
+    hash.update(secret + data + secret);
+    return hash.digest("hex").toString();
+};
+
+app.get("/checkWhitelist", (request, response) => {
+    const Key = request.query.Key;
+    const Gamer = request.query.gamer;
+
+    if(Key && Gamer){
+        const isKeyValid = Keys.find((key) => key !== null && Key == key);
+
+        if(isKeyValid){
+            response.send(hmac(secretKey2, Key + Gamer)) // we'll send the data of the whitelist response to the client once the key is valid
+        }
+        else{
+            response.send("Not Whitelisted");
+        }
+    }
+    else{
+        response.send("Not Whitelisted");
+    }
+});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
